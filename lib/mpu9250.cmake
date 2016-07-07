e19a1fc84b9aefd67014e48b48636deac0eaece4
Ryan Kurte <ryankurte@gmail.com>
2016-07-07 15:54:02 +1200

    Added registers, cmake files. Not sure whether to implement tests here or not, will probably end up mocking out a device.

---
+++ lib/mpu9250.cmake
@@ -0,0 +1,14 @@
+# Libmpu9250 cmake include file
+
+# Add library includes
+include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
+
+# Add project sources
+set(LIBMPU9250_SOURCES
+    ${CMAKE_CURRENT_LIST_DIR}/source/mpu9250.c
+)
+
+# Create library
+add_library(mpu9250 ${LIBMPU9250_SOURCES})
+
+set(OPTIONAL_LIBS mpu9250 ${OPTIONAL_LIBS})
